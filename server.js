b247615fcb3a802b967e13af93a4086b0632c277
srushti181203 <srushti181203@gmail.com>
2024-11-10 12:46:21 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const index = require('./index');
+const { hostname } = require('os');
+
+const port= process.env.PORT || 5000;
+const server = http.createServer(index);
+
+server.listen(port, hostname, () => {
+    console.log(`started on port ${port}`);
+});
