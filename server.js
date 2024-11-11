55883f436dc4173f4402b91a6a645b605e7dd277
srushti181203 <srushti181203@gmail.com>
2024-11-11 20:20:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const index = require('./index');
 const { hostname } = require('os');
 
-const port= process.env.PORT || 5000;
+const port= process.env.PORT || 4000;
 const server = http.createServer(index);
 
 server.listen(port, "0.0.0.0", () => {
