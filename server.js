1dd8679c9a52c2a771da6e0c994216c49fcc4be4
srushti181203 <srushti181203@gmail.com>
2024-11-11 20:51:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const { hostname } = require('os');
 const port= process.env.PORT || 5000;
 const server = http.createServer(index);
 
-server.listen(port, '0.0.0.0', () => {
+server.listen(port,  () => {
     console.log(`started on port ${port}`);
 });
