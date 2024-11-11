a642c4996b1b7ba3788cd7904d1aaf75499e4ba4
srushti181203 <srushti181203@gmail.com>
2024-11-11 20:38:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const index = require('./index');
 const port= process.env.PORT || 5000;
 const server = http.createServer(index);
 
-server.listen(port, "0.0.0.0", () => {
+server.listen(port, '0.0.0.0', () => {
     console.log(`started on port ${port}`);
 });
